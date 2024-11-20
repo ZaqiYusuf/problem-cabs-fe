4d21d08b9b5b7c8ffdd083670003312be4309fc7
FawwazZah13 <fwwzzahran@gmail.com>
2024-11-21 03:44:49 +0700

    Update di bagian api_helpers.tsx

--- src/helpers/api_helper.ts
+++ src/helpers/api_helper.ts
@@ -3,31 +3,39 @@ import getErrorMessage from "./getErrorMessage";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000/api",
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: "http://localhost:8000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-// Content type
+// Content type untuk POST
 api.defaults.headers.post["Content-Type"] = "application/json";
 
-// Set authorization token if available
-const token = localStorage.getItem("accessToken");
-if (token){
-  api.defaults.headers.common["Authorization"] = "Bearer " + token;
+/**
+ * Interceptors Add Request
+ */
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
 
-} else{
-  api.defaults.headers.common["Authorization"] = "";
-}
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
 
-// Interceptor to handle responses and errors
+    return config;
+  },
+  (err) => Promise.reject(err)
+);
+
+// Interceptor untuk handle response dan error
 api.interceptors.response.use(
-  function (response) {
+  (response) => {
     return response.data ? response.data : response;
   },
-  function (error) {
-    console.log("api", error);
+  (error) => {
+    console.log("API Error:", error);
+
     if (error.response) {
       if (error.response.status === 401) {
         localStorage.removeItem("authUser");
@@ -37,83 +45,61 @@ api.interceptors.response.use(
       }
 
       if (error.response.status === 403) {
-        // Show toast notification on 403 Forbidden
         toast.error("You do not have permission to access this resource.");
         return Promise.reject(error);
       }
     }
 
-    let message = getErrorMessage(error);
+    const message = getErrorMessage(error);
     return Promise.reject(message);
   }
 );
 
 /**
  * Sets the default authorization
- * @param {*} token
+ * @param {string} token
  */
-const setAuthorization = (token: any) => {
-  api.defaults.headers.common["Authorization"] = "Bearer " + token;
+const setAuthorization = (token: string) => {
+  api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 };
 
 class APIClient {
   /**
    * Fetches data from given url
    */
-  get = (url: any, params: any) => {
-    let response;
-
-    let paramKeys: any = [];
-
+  get = (url: string, params?: Record<string, any>) => {
     if (params) {
-      Object.keys(params).map((key) => {
-        paramKeys.push(key + "=" + params[key]);
-        return paramKeys;
-      });
-
-      const queryString =
-        paramKeys && paramKeys.length ? paramKeys.join("&") : "";
-      response = api.get(`${url}?${queryString}`, params);
+      const queryString = new URLSearchParams(params).toString();
+      return api.get(`${url}?${queryString}`);
     } else {
-      response = api.get(`${url}`, params);
+      return api.get(url);
     }
-
-    return response;
   };
 
   /**
    * Posts given data to url
    */
-  create = (url: any, data: any) => {
-    return api.post(url, data);
-  };
+  create = (url: string, data: any) => api.post(url, data);
 
   /**
    * Updates data
    */
-  update = (url: any, data: any) => {
-    return api.patch(url, data);
-  };
+  update = (url: string, data: any) => api.patch(url, data);
 
-  put = (url: any, data: any) => {
-    return api.put(url, data);
-  };
+  put = (url: string, data: any) => api.put(url, data);
 
   /**
    * Delete
    */
-  delete = (url: any, config: any) => {
-    return api.delete(url, { ...config });
-  };
+  delete = (url: string, config?: any) => api.delete(url, { ...config });
 }
 
+/**
+ * Get logged user data from localStorage
+ */
 const getLoggedUser = () => {
   const user = localStorage.getItem("authUser");
-  if (!user) {
-    return null;
-  } else {
-    return JSON.parse(user);
-  }
+  return user ? JSON.parse(user) : null;
 };
 
 export { APIClient, setAuthorization, getLoggedUser };
